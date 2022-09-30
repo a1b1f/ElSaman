13d4ee4000231dae034cb16413dcf5e6afafb6c9
Ahmed Rafie <ahmedrafie6@gmail.com>
2022-09-30 18:23:31 +0200

    Home,nav,footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { FavComponent } from './Components/fav/fav.component';
 import { CartComponent } from './Components/cart/cart.component';
 import { ProductComponent } from './Components/product/product.component';
 import { OrderComponent } from './Components/order/order.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { OrderComponent } from './Components/order/order.component';
     FavComponent,
     CartComponent,
     ProductComponent,
-    OrderComponent
+    OrderComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
