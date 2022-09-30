4ec5a6f2da78d779be252d12cbb42b45390de6a4
Ahmed Rafie <ahmedrafie6@gmail.com>
2022-09-30 18:31:35 +0200

    Routing,nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './Components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"Home",pathMatch:"full"},
+  {path:"Product",component:ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
