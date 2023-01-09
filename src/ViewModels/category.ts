494f344d28925af0688075c928cd9a8830bab1fe
Basma Esmail <BasmaE@GSTechnologyCo.com>
2023-01-09 02:33:15 +0200

    fix fliter error & nav style

--- src/ViewModels/category.ts
+++ src/ViewModels/category.ts
@@ -1,6 +1,7 @@
 export class Category {
   nameEN:string="";
   nameAR:string="";
+  id:number=0;
   isDeleted:boolean= false;
   isChecked:boolean=false;
 }
--- src/app/Components/Product/product-list/product-list.component.ts
+++ src/app/Components/Product/product-list/product-list.component.ts
@@ -18,8 +18,10 @@ export class ProductListComponent implements OnInit {
   tableSizes: any = [1, 5, 10, 20];
   Products:Product[]=[];
   unfiltered:Product[]=[];
+  filtered:Product[]=[];
   Categories:Category[]=[];
   productName:string="";
+  // selected:number[]=[];
   constructor( private ProductService: productservice) { }
 
   ngOnInit(): void {
@@ -28,21 +30,22 @@ export class ProductListComponent implements OnInit {
   fetchData() {
     //console.log(this.tableSize,this.page)
     this.ProductService.getProduct(this.tableSize,this.page).subscribe(res => {
-     // console.log(res.data.data)
+     console.log(res.data)
 
       let response = res.data as PagingViewModel
       this.page = response.pageIndex;
       this.tableSize = response.pageSize;
       this.count = response.count;
       this.Products = response.data as Product[];
+      this.filtered=this.Products;
       this.getProductByName();
       this.ProductService.getCategories().subscribe(res=>
         {
           console.log(res)
           this.Categories=res.data ;
-          var c = new Category();
-          c.nameAR="الكل";
-          this.Categories.push(c);
+          // var c = new Category();
+          // c.nameAR="الكل";
+          // this.Categories.push(c);
         })
     })
   }
@@ -63,8 +66,8 @@ export class ProductListComponent implements OnInit {
         {
         this.ProductService.getProductByName(this.productName).subscribe(res=>
           {
-            //console.log(res);
-            this.Products=res.data.data
+            // console.log(res);
+            this.filtered=res.data.data
 
             // this.Products=this.unfiltered.filter(i=>i.isDeleted == false)
           })
@@ -79,37 +82,74 @@ export class ProductListComponent implements OnInit {
 
         }
   }
-  getByCategory(cName:string){
-    this.Categories.filter(i=>{
-      if(i.nameAR==cName)
-      {
-        i.isChecked=!i.isChecked
-      }
-    })
-    this.Products=[];
-    this.Categories.forEach(c=>{
-      if(c.isChecked==true )
-      {
-        if(c.nameAR=="الكل"){
-          this.ProductService.getProduct(this.tableSize,this.page).subscribe(res => {
-            let response = res.data as PagingViewModel
-            this.page = response.pageIndex;
-            this.tableSize = response.pageSize;
-            this.count = response.count;
-            this.Products = response.data as Product[];
-            this.Products= this.Products.filter(i=>  i.nameAR.includes(this.productName))
-          })
-        }
-        else {
-        this.ProductService.getByCategory(c.nameAR).subscribe(res=>
-          {
-            //console.log(res);
-            this.unfiltered=res.data.data
-            this.Products.push(...this.unfiltered.filter(i=>  i.nameAR.includes(this.productName)))
-          })
-        }
+  getByCategory(cId:any){
+    // this.Categories.filter(i=>{
+    //   if(i.nameAR==cName)
+    //   {
+    //     i.isChecked=!i.isChecked
+    //   }
+    // })
+    // this.Products=[];
+    // this.Categories.forEach(c=>{
+    //   if(c.isChecked==true )
+    //   {
+    //     if(c.nameAR=="الكل"){
+    //       this.ProductService.getProduct(this.tableSize,this.page).subscribe(res => {
+    //         let response = res.data as PagingViewModel
+    //         this.page = response.pageIndex;
+    //         this.tableSize = response.pageSize;
+    //         this.count = response.count;
+    //         this.Products = response.data as Product[];
+    //         this.Products= this.Products.filter(i=>  i.nameAR.includes(this.productName))
+    //       })
+    //     }
+    //     else {
+    //     this.ProductService.getByCategory(c.nameAR).subscribe(res=>
+    //       {
+    //         //console.log(res);
+    //         this.unfiltered=res.data.data
+    //         this.Products.push(...this.unfiltered.filter(i=>  i.nameAR.includes(this.productName)))
+    //       })
+    //     }
 
+    //   }
+    // })
+    // console.log(cId);
+    // if(!this.selected.includes(cId))
+    // {this.selected.push(cId)
+    //   this.selected.forEach(s=>{
+
+    //     this.filtered=this.Products.filter(p=>parseInt(p.categoryID)==s)
+    //   })
+    // }
+    // else
+    // {
+    //   this.filtered=this.Products
+    //   this.selected=[]
+    // }
+    // console.log(this.selected);
+
+    this.filtered = [];
+
+    //We are assigning the selected cat products to the product list and if no cat is selected nothing happens
+    for (var i = 0; i < this.selected.length; i++) {
+      var lst = this.Products.filter(x => parseInt(x.categoryID) == this.selected[i].id);
+      for (var j = 0; j < lst.length; j++) {
+        this.filtered.push(lst[j]);
       }
-    })
+      this.filtered = this.filtered.filter(p=>p.nameAR.includes(this.productName));
+    }
+
+    //If no checkbox is selected assign original product list to product list
+    if (this.selected.length == 0) {
+      this.filtered = this.Products.filter(p=>p.nameAR.includes(this.productName));
+    }
+    
   }
+
+  get selected() {
+    //Get all the selected brands
+    return this.Categories.filter(opt => opt.isChecked)
+  }
+
 }
